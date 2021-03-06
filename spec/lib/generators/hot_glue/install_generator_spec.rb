fbed5b00f7b9b755b53eeeab9f66695f9f5901a0
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-03-06 13:41:09 -0500

    adding stubs for specs

--- spec/lib/generators/hot_glue/install_generator_spec.rb
+++ spec/lib/generators/hot_glue/install_generator_spec.rb
@@ -13,10 +13,30 @@ describe HotGlue::InstallGenerator do
     $LOAD_PATH.delete(@path)
   end
 
-  it "with missing object name should give error" do
 
-    response = Rails::Generators.invoke("hot_glue:install")
-    expect(File.exist?("spec/dummy/app/views/layouts/_flash_notices.haml")).to be(true)
+  describe "ERROR RESPONESES" do
+    it "with missing object name should give error" do
+
+      response = Rails::Generators.invoke("hot_glue:install")
+      expect(File.exist?("spec/dummy/app/views/layouts/_flash_notices.haml")).to be(true)
+    end
+
+    it "with no auth an no current user should give me error" do
+
+
+    end
+
+    it "with no auth no current user and no auth identifier" do
+
+    end
+
+    it "with xxx" do
+
+    end
+  end
+
+  describe "GOOD RESPONSES" do
+    # test good builds here
   end
 end
 
